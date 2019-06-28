8664d3def1937bee49c7a83f8602beb4dad049c6
fcarlone <frankcarlone@gmail.com>
2019-06-28 12:21:12 -0400

    Display Google Books API

--- routes/books.js
+++ routes/books.js
@@ -1,6 +1,18 @@
+const axios = require("axios");
+console.log(axios);
 // Routes
 module.exports = function(app) {
-  app.get("/api/books", (req, res) => {
-    res.json({ msg: "books api" });
+  // GET Request
+  app.get("/api/books", async (req, res) => {
+    // res.json({ msg: "books api" });
+
+    console.log("test google api request");
+
+    const response = await axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=moby%20dick"
+    );
+
+    console.log("response:", response);
+    res.send(response.data.items[0].volumeInfo);
   });
 };
