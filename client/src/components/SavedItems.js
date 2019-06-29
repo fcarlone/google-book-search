55ec49b39f945aca14bfa074ee4b14b31cf0b5a1
fcarlone <frankcarlone@gmail.com>
2019-06-29 11:56:06 -0400

    Get remove button to delete a book from database

--- client/src/components/SavedItems.js
+++ client/src/components/SavedItems.js
@@ -1,10 +1,21 @@
 import React from "react";
+import axios from "axios";
 
 const SavedItems = ({ book }) => {
   const { title, authors, description, link, image, _id } = book;
 
   const handleDeleteBook = event => {
-    console.log("delete button", event.target.value);
+    const id = event.target.value;
+    console.log("delete button", id);
+
+    // axois call
+    axios
+      .delete(`/api/books/${id}`, (req, res) => {
+        console.log("test");
+      })
+      .then(function(response) {
+        console.log(response);
+      });
   };
   return (
     <div>
