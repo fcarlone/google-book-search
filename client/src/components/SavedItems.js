c08e9729253b1e32d9e48f13b62c91884a44bb7d
fcarlone <frankcarlone@gmail.com>
2019-06-29 11:42:00 -0400

    Get database id for book to remove from saved items

--- client/src/components/SavedItems.js
+++ client/src/components/SavedItems.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SavedItems = ({ book }) => {
-  const { title, authors, description, link, image } = book;
+  const { title, authors, description, link, image, _id } = book;
+
+  const handleDeleteBook = event => {
+    console.log("delete button", event.target.value);
+  };
   return (
     <div>
       <h1>SavedItem Component</h1>
@@ -17,7 +21,14 @@ const SavedItems = ({ book }) => {
         </a>
       </div>
       <div>
-        <button>Remove</button>
+        <button
+          value={_id}
+          onClick={event => {
+            handleDeleteBook(event);
+          }}
+        >
+          Remove{""}
+        </button>
       </div>
     </div>
   );
