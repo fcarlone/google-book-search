52461148cc3e40014d6e0147373d89f3a7a05831
fcarlone <frankcarlone@gmail.com>
2019-08-13 15:34:42 -0400

    Create handleRemoveSavedBooks for Saved components

--- client/src/components/Saved.js
+++ client/src/components/Saved.js
@@ -27,6 +27,18 @@ class Saved extends React.Component {
   handleRemoveSavedBook = id => {
     console.log("test");
     console.log("handle remove saved book", id);
+
+    // Update database
+    axios.delete(`/api/books/${id}`).then(response => {
+      console.log("saved book deleted: ", response.data);
+    });
+
+    // Update state
+    let result = this.state.books.filter(book => {
+      return book._id !== id;
+    });
+
+    this.setState({ books: result });
   };
 
   render() {
